46c0a2e2d62c68bcfcd2956c3aeaf0a88b5ccdc5
Eduardo Carvalho <eduardotimmers@gmail.com>
2020-04-17 12:48:59 -0300

    Updates

---
+++ precache-manifest.0d8b6cacffa2a52c9c30ee8c3f6abe75.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46044fa501a86444caffe923718ac929",
+    "url": "/frontloops/index.html"
+  },
+  {
+    "revision": "52f01677b9d6d79dbf9d",
+    "url": "/frontloops/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bab58f5ad761a3642dcc",
+    "url": "/frontloops/static/js/2.e9c03b60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontloops/static/js/2.e9c03b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f01677b9d6d79dbf9d",
+    "url": "/frontloops/static/js/main.99c4a088.chunk.js"
+  },
+  {
+    "revision": "18c545723761c69dd0f0",
+    "url": "/frontloops/static/js/runtime-main.9da1136a.js"
+  },
+  {
+    "revision": "346bbfda497dc3b39dcd178fee31f2cb",
+    "url": "/frontloops/static/media/account.346bbfda.svg"
+  },
+  {
+    "revision": "380aa4fc180742618f851475a9116c3e",
+    "url": "/frontloops/static/media/arrow.380aa4fc.svg"
+  },
+  {
+    "revision": "cdbf354270f84708e4f21c61d171e274",
+    "url": "/frontloops/static/media/bicycle.cdbf3542.png"
+  },
+  {
+    "revision": "f611c0517e82ceed5815a05ea84dfc9f",
+    "url": "/frontloops/static/media/check.f611c051.svg"
+  },
+  {
+    "revision": "7b66831ce156de757d9bc34d4744da93",
+    "url": "/frontloops/static/media/close.7b66831c.svg"
+  },
+  {
+    "revision": "a6e91886405b3cab9e748f2428549120",
+    "url": "/frontloops/static/media/device.a6e91886.svg"
+  },
+  {
+    "revision": "c1356d39a79e9c2c9bcf903ba3062a9e",
+    "url": "/frontloops/static/media/github.c1356d39.svg"
+  },
+  {
+    "revision": "54863705a06a7a2e6380fbfa2763d470",
+    "url": "/frontloops/static/media/laptop.54863705.svg"
+  },
+  {
+    "revision": "f7b18d21089b61cc3c510ab6d7da2cb5",
+    "url": "/frontloops/static/media/linkedin.f7b18d21.svg"
+  },
+  {
+    "revision": "10658eed26191156a105dc11585d336c",
+    "url": "/frontloops/static/media/maestro.10658eed.svg"
+  },
+  {
+    "revision": "8372b94426e9f73d98213a5379750040",
+    "url": "/frontloops/static/media/mastercard.8372b944.svg"
+  },
+  {
+    "revision": "ef7c9e48e6b83fd8ac5a33c6c6489c52",
+    "url": "/frontloops/static/media/monitor.ef7c9e48.svg"
+  },
+  {
+    "revision": "89eaa7422b26701cdfabbdf4fde0e5ee",
+    "url": "/frontloops/static/media/twitter.89eaa742.svg"
+  },
+  {
+    "revision": "1453b285d599205c2dbca57485e93dcf",
+    "url": "/frontloops/static/media/visa.1453b285.svg"
+  }
+]);
